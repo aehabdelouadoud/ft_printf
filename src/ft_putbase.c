9a694a90e6450c5fa40d86700aadc68f86321d45
AIT EL HAJ Abdelouadoud <152083466+aehabdelouadoud@users.noreply.github.com>
2025-11-03 05:09:27 +0100

    Update on Mon Nov  3 05:09:27 AM +01 2025

--- src/ft_putbase.c
+++ src/ft_putbase.c
@@ -6,7 +6,7 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 01:51:11 by abait-el          #+#    #+#             */
-/*   Updated: 2025/11/03 04:43:23 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/11/03 05:06:47 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,12 @@ void	ft_putbase(long long n, char *base)
 		ft_putstr("-9223372036854775808");
 		return ;
 	}
-	if (n > (long long)len)
+	if (n < 0)
+	{
+		n = -n;
+		ft_putchar('-');
+	}
+	if (n >= (long long)len)
 		ft_putbase(n / len, base);
 	ft_putchar(*(base + (n % len)));
 }
--- src/ft_putubase.c
+++ src/ft_putubase.c
@@ -6,7 +6,7 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 01:51:11 by abait-el          #+#    #+#             */
-/*   Updated: 2025/11/03 04:46:10 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/11/03 05:09:10 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putubase(unsigned long long n, char *base)
 	size_t	len;
 
 	len = ft_strlen(base);
-	if (n > (unsigned long long)len)
+	if (n >= (unsigned long long)len)
 		ft_putbase(n / len, base);
 	ft_putchar(*(base + (n % len)));
 }
