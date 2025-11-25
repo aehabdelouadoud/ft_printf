aaf3cab962ed51c576a1e3b1aaa92a5eafe388dd
AIT EL HAJ Abdelouadoud <152083466+aehabdelouadoud@users.noreply.github.com>
2025-11-25 04:15:31 +0100

    Remove .cache

--- src/ft_putbase.c
+++ src/ft_putbase.c
@@ -6,15 +6,15 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 01:51:11 by abait-el          #+#    #+#             */
-/*   Updated: 2025/11/10 00:35:16 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/11/25 04:14:14 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-ssize_t	ft_putbase(const long long n, char *base)
+ssize_t	ft_putbase(const t_ll n, char *base)
 {
 	if (n < 0)
-		return (ft_putchar('-'), 1 + ft_putubase(-(unsigned long long)n, base));
+		return (ft_putchar('-'), 1 + ft_putubase(-(t_ull)n, base));
 	return (ft_putubase(n, base));
 }
--- src/ft_putubase.c
+++ src/ft_putubase.c
@@ -6,18 +6,18 @@
 /*   By: abait-el <abait-el@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 01:51:11 by abait-el          #+#    #+#             */
-/*   Updated: 2025/11/03 11:01:54 by abait-el         ###   ########.fr       */
+/*   Updated: 2025/11/25 04:14:49 by abait-el         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-ssize_t	ft_putubase(unsigned long long n, char *base)
+ssize_t	ft_putubase(t_ull n, char *base)
 {
 	ssize_t	r;
 
 	r = 0;
-	if (n >= (unsigned long long)ft_strlen(base))
+	if (n >= (t_ull)ft_strlen(base))
 		r = ft_putubase(n / ft_strlen(base), base);
 	ft_putchar(base[n % ft_strlen(base)]);
 	return (1 + r);
